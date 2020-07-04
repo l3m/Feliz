555f307ea66662238cf314ccb3bd6b8058133819
Jonas Bösch <jb@vlot.ch>
2020-07-04 14:59:25 +0200

    Added UseMediaQuery
    - UseMediaQuery allows the use of an arbitrary media query
    - UseResponsive uses the of predefined or specified breakpoints
      to determine the screen size

---
+++ Feliz.UseMediaQuery/UseMediaQuery.fs
@@ -0,0 +1,65 @@
+﻿module Feliz.UseMediaQuery
+
+open System
+open Browser
+open Browser.Types
+open Feliz
+
+type ScreenSize =
+    | Mobile
+    | MobileLandscape
+    | Tablet
+    | Desktop
+    | WideScreen
+
+type Breakpoints = {
+    MobileLandscape: int
+    Tablet: int
+    Desktop: int
+    WideScreen: int
+}
+
+let defaultBreakpoints = {
+    MobileLandscape = 576
+    Tablet = 768
+    Desktop = 1024
+    WideScreen = 1216
+}
+
+let makeQueries breakpoints =
+    let mobileQuery = sprintf "(max-width: %ipx)" breakpoints.MobileLandscape
+    let mobileLandscapeQuery = sprintf "(max-width: %ipx)" breakpoints.Tablet
+    let tabletQuery = sprintf "(max-width: %ipx)" breakpoints.Desktop
+    let desktopQuery = sprintf "(max-width: %ipx)" breakpoints.WideScreen
+    mobileQuery, mobileLandscapeQuery, tabletQuery, desktopQuery
+
+[<AutoOpen>]
+module UseMediaQueryExtension =
+     type React with
+        static member useMediaQuery (query: string) =
+            let (mq, setMq) = React.useState(fun () -> window.matchMedia(query))
+
+            React.useEffect(fun () ->
+                mq.addEventListener("change", fun e ->
+                    let mqEvent = unbox<MediaQueryList>(e)
+                    setMq mqEvent)
+                {new IDisposable with
+                     member this.Dispose() =
+                        mq.removeEventListener("change", fun _ -> ())}
+
+            , [| query :> obj |])
+
+            mq.matches
+
+        static member useResponsive (breakpoints: Breakpoints) =
+            let m, l, t, d = makeQueries breakpoints
+            let mx = React.useMediaQuery m
+            let lx = React.useMediaQuery l
+            let tx = React.useMediaQuery t
+            let dx = React.useMediaQuery d
+            match mx, lx, tx, dx with
+            | true, _, _, _ -> ScreenSize.Mobile
+            | _, true, _, _ -> ScreenSize.MobileLandscape
+            | _, _, true, _ -> ScreenSize.Tablet
+            | _, _, _, true -> ScreenSize.Desktop
+            | _ -> ScreenSize.WideScreen
